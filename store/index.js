59f0daa03aae356c582da5b027fb17fae0f76204
ToshifumiIde <toshifumiide198765@gmail.com>
2020-11-30 22:41:46 +0900

    スプラッシュスクリーンの実装（途中）

--- store/index.js
+++ store/index.js
@@ -1,13 +1,25 @@
 import { createStore , combineReducers } from "redux";
 import userReducer from "./reducers/user";
 import {composeWithDevTools} from "redux-devtools-extension";
+import { persistReducer, persistStore } from "redux-persist"
+import { AsyncStorage } from "react-native";
 
 //reducerをまとめたrootReducerの生成
 const rootReducer = combineReducers({
   user:userReducer,
 });
 
-//rootReducerを第一引数に、デバッガーツールの実行を第二引数にしてstoreを生成
-const store = createStore(rootReducer , composeWithDevTools());
+const persistConfig = {
+  key:"root",
+  storage: AsyncStorage,
+  whitelist:["user"],
+};
 
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+//rootReducerを第一引数に、デバッガーツールの実行を第二引数にしてstoreを生成
+const store = createStore(persistedReducer , composeWithDevTools());
+
+export const persistor = persistStore(store);
 export default store;
